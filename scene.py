b2a9948d0a5f2be09b8b654d7a4d18f403780894
Mateusz Nikiel <pascal.nikiel@gmail.com>
2021-12-22 16:23:01 -0500

    refactor: extract entities from scene

--- scene.py
+++ scene.py
@@ -1,9 +1,9 @@
 import io
 import yaml
 
-def turn_off_scene(scene_entity):
-  lights = [entity for entity in scene_entity['entities'] if entity.startswith('light.')]
-  switches = [entity for entity in scene_entity['entities'] if entity.startswith('switch.')]
+def turn_off_entities(scene_entities):
+  lights = [entity for entity in scene_entities if entity.startswith('light.')]
+  switches = [entity for entity in scene_entities if entity.startswith('switch.')]
 
   for light_id in lights:
     light.turn_off(entity_id=light_id)
@@ -16,9 +16,9 @@ def toggle_scene(scene_name):
   scene_id = state.getattr(scene_name)['id']
   with io.open('scenes.yaml') as scenes_file:
     scenes = yaml.safe_load(scenes_file)
-    scene_entity = [scene for scene in scenes if scene['id'] == scene_id][0]
+    scene_entities = [scene['entities'] for scene in scenes if scene['id'] == scene_id][0]
 
-    if any([state.get(entity_id) == 'on' for entity_id in scene_entity['entities']]):
-      turn_off_scene(scene_entity)
+    if any([state.get(entity_id) == 'on' for entity_id in scene_entities]):
+      turn_off_entities(scene_entities)
     else:
       scene.turn_on(entity_id=scene_name)
