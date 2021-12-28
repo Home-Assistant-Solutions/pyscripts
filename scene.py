1c46253fa246137f484c1eec2d6536288fd11574
Mateusz <pascal.nikiel@gmail.com>
2021-12-28 10:26:22 -0500

    feat: add turn off scene script

--- scene.py
+++ scene.py
@@ -12,6 +12,14 @@ def turn_off_entities(scene_entities):
     switch.turn_off(entity_id=switch_id)
 
 @service
+def turn_off_scene(scene_name):
+  scene_id = state.getattr(scene_name)['id']
+  with io.open('scenes.yaml') as scenes_file:
+    scenes = yaml.safe_load(scenes_file)
+    scene_entities = [scene['entities'] for scene in scenes if scene['id'] == scene_id][0]
+    turn_off_entities(scene_entities)
+
+@service
 def toggle_scene(scene_name):
   scene_id = state.getattr(scene_name)['id']
   with io.open('scenes.yaml') as scenes_file:
